09214e928801dec943cbc12ea1bc0e19985bea95
rahqan.shaikh@aurionpro.com <rahqan.shaikh@aurionpro.com>
2025-10-26 18:25:20 +0530

    commit some more files

---
+++ Banking-Payments/Models/ContactDetails.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dummy_api.Models
+{
+    public class ContactDetails
+    {
+        [Key]
+        public int ContactDetailsId { get; set; }
+
+        [Required(ErrorMessage ="Contact Person Name is Required")]
+        public string ContactName { get; set; }
+
+        [Required(ErrorMessage ="Contact Number is Requied")]
+        public string ContactNumber { get; set; }
+
+        [Required(ErrorMessage ="Contact Email is Required")]
+        public string ContactEmail { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        [ForeignKey("Bank")]
+        public int BankId { get; set; }
+
+        public Bank? Bank { get; set; }
+    }
+}
