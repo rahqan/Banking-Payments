dc627ec50ac4c646134c8597e3eb3c1bf8e868d0
jatin-pandey01 <jatinpandey3250@gmail.com>
2025-10-31 14:21:12 +0530

    Added untranced files

--- obj/Debug/net8.0/dummy_api.AssemblyInfo.cs
+++ obj/Debug/net8.0/dummy_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dummy_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7d31735e99423ed4eb3d6f3d2d6dd52dbf4821b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3055eccec45e8e217a66489bc14fdd2fd202d699")]
 [assembly: System.Reflection.AssemblyProductAttribute("dummy_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dummy_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
