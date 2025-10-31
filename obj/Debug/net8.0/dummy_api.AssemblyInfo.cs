472657c8b2cde2db3650c18b680e2d4793f54c52
jatin-pandey01 <jatinpandey3250@gmail.com>
2025-10-31 17:40:14 +0530

    Merge fixed

--- obj/Debug/net8.0/dummy_api.AssemblyInfo.cs
+++ obj/Debug/net8.0/dummy_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dummy_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b80b3fd94c40f1a6abbe572024de1ee65a036d97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aeea94bbcdfb3ac7c90263e1c2fc66da6207345")]
 [assembly: System.Reflection.AssemblyProductAttribute("dummy_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dummy_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
